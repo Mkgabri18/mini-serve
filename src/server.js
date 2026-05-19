c84621b7001432196100deb232f0655633daf639
mkgabri18 <carrasco.gabriel136@hotmail.it>
2026-05-20 01:06:17 +0200

    refactor: update server creation to use 'node:http' module

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-import http from "http";
+import { createServer } from 'node:http';
 import { createApp } from "./app.js";
 
 const app = createApp();
 
-const server = http.createServer(app);
+const server = createServer(app);
 
 server.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
